cefa44f7a24712d8d4959885a358755e075973ac
Spaghetti Jones <connor.breault@comcast.net>
2021-09-28 23:41:50 -0700

    background and color scheme changed

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,32 +2,28 @@ const imgRoute = "./assets/images/projects";
 var projects = [
   {
     name: "Record Label",
-    info:
-      "Label site built for clients Dead Life Records!  Full stack app utilizing AMP, mongoDB, and emailJS.  Storefront developed using shopify. Sole Developer and Designer.",
+    info: "Label site built for clients Dead Life Records!  Full stack app utilizing AMP, mongoDB, and emailJS.  Storefront developed using shopify. Sole Developer and Designer.",
     githubRepo: "https://github.com/connorbreault/DeadLifeRecords",
     liveSite: "https://deadliferecords.com/",
     image: `${imgRoute}/DeadLife.png`,
   },
   {
     name: "Flub",
-    info:
-      "Simplistic site built for clients in the band Flub!  Full stack app utilizing Firebase and emailJS. Sole Developer.",
+    info: "Simplistic site built for clients in the band Flub!  Full stack app utilizing Firebase and emailJS. Sole Developer.",
     githubRepo: "https://github.com/connorbreault/Flub",
     liveSite: "https://flubmusic.com/",
     image: `${imgRoute}/Flub.png`,
   },
   {
     name: "BB quiz",
-    info:
-      "Which Bobs Burgers character are you?  Take this short quiz to find out!  Full stack app deployed through Heroku. Sole Developer.",
+    info: "Which Bobs Burgers character are you?  Take this short quiz to find out!  Full stack app deployed through Heroku. Sole Developer.",
     githubRepo: "https://github.com/connorbreault/Friend-Finder",
     liveSite: "https://friend-finder-breault.herokuapp.com/",
     image: `${imgRoute}/Friend-finder.png`,
   },
   {
     name: "Burger app",
-    info:
-      "Full stack app using mySQL and deployed through Heroku. Sole Developer.",
+    info: "Full stack app using mySQL and deployed through Heroku. Sole Developer.",
     githubRepo: "https://github.com/connorbreault/burger",
     liveSite: "https://nameless-mesa-44263.herokuapp.com/",
     image: `${imgRoute}/Burger.png`,
@@ -49,16 +45,14 @@ var projects = [
   },
   {
     name: "Excursion",
-    info:
-      "Faux Travel budget app,  group project 1.  Co-lead developer of a group of 4. I created the entire visual layout.  Currently deployed through Github.",
+    info: "Faux Travel budget app,  group project 1.  Co-lead developer of a group of 4. I created the entire visual layout.  Currently deployed through Github.",
     githubRepo: "https://github.com/connorbreault/Excursion",
     liveSite: "https://connorbreault.github.io/Excursion/",
     image: `${imgRoute}/Excursion.png`,
   },
   {
     name: "WeHarmony",
-    info:
-      "Find local musicians, group project 3.  Lead Developer of a group of 2.  I set up the database, all API calls, and created the entire front end.  Deployed through Heroku.",
+    info: "Find local musicians, group project 3.  Lead Developer of a group of 2.  I set up the database, all API calls, and created the entire front end.  Deployed through Heroku.",
     githubRepo: "https://github.com/connorbreault/WeHarmonyPrototype",
     liveSite: "https://weharmonyprototype.herokuapp.com/",
     image: `${imgRoute}/WeHarmony.png`,
@@ -92,7 +86,7 @@ function renderProjects() {
         <img src=${projects[i].image} class="activator cardImg" style="height: 200px">
     </div>
     <div class="card-content activator" style="padding: 15px;">
-      <span class="card-title activator white-text">${projects[i].name}
+      <span class="card-title activator amber-text text-lighten-3">${projects[i].name}
         <i class="material-icons right">more_vert</i>
       </span>
     </div>
